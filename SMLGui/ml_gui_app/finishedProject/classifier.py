6c73c1d208562fd14c5bf5e63dc6d07df9e1759e
Andrea Prestini <andrea.prestini@gmail.com>
2023-11-26 17:57:18 +0100

    style restyling !nothing: apply linter flake8 to file classifier.py

--- SMLGui/ml_gui_app/finishedProject/classifier.py
+++ SMLGui/ml_gui_app/finishedProject/classifier.py
@@ -18,6 +18,7 @@ class_names = {
 
 model = models.load_model("baseline_mariya.keras")
 
+
 def predict_image(model, path_to_img):
     img = Image.open(path_to_img)
     img = img.convert("RGB")
@@ -28,9 +29,10 @@ def predict_image(model, path_to_img):
 
     top_prob = probs.max()
     top_pred = class_names[np.argmax(probs)]
-    
+
     return top_prob, top_pred
-    
+
+
 content = ""
 img_path = "placeholder_image.png"
 prob = 0
@@ -51,17 +53,14 @@ select an image from your file system
 >
 """
 
+
 def on_change(state, var_name, var_val):
     if var_name == "content":
         top_prob, top_pred = predict_image(model, var_val)
         state.prob = round(top_prob * 100)
         state.pred = "this is a " + top_pred
         state.img_path = var_val
-    #print(var_name, var_val)
-
-
-
-
+    # print(var_name, var_val)
 
 
 app = Gui(page=index)
