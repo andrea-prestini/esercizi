9c4a76643b875d817c2a7afd5b7e20613ccc0976
Andrea Prestini <andrea.prestini@gmail.com>
2023-11-27 13:44:58 +0100

    add: test2.py
    mod: test2.py test1.py
    
    mod: test1.py
    
    mod: test2.py
    
    mod: test1.py
    
    mod: test1.py
    
    mod: test2.py

--- test/test1.py
+++ test/test1.py
@@ -7,13 +7,17 @@ class Persona:
         self.name = name
 
     def __str__(self):
-        print(f"""
+        return f"""
         Sono {self.name}
-        e sono in {self.zona}""")
+        e sono in {self.zona}"""
 
     def set_zona(self, new_zona: str) -> None:
         self.zona = new_zona
 
 
-time.sleep(2)
+obj = Persona("andrea prestini")
+obj1 = Persona("mario rossi")
+
+print(obj)
+
 print("fine procedura...")
