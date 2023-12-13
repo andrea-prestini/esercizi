b440117fa0df8ef6b7304a959c2fa0050c7450ea
Andrea Prestini <andrea.prestini@gmail.com>
2023-12-13 12:16:19 +0100

    mod: flatpak Pollini
    secondo step

--- Pollini/flask-todo-list/app.py
+++ Pollini/flask-todo-list/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from markupsafe import escape
 
 
@@ -7,31 +7,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Index page'
-
-
-@app.route('/hello')
-def hello():
-    return "Ciao mondo infame!"
-
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return f'User {escape(username)}'
-
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return f'Post {post_id}'
-
-
-@app.route('/projects/')
-def projects():
-    return 'The project page'
-
-
-@app.route('/about')
-def about():
-    return 'The about page'
+    welcome_message = 'Welcome to BICHER'
+    return render_template('index.html', message=welcome_message)
