91090dbb4d614ba7f100e6eb421500aa694a1b71
Andrea Prestini <andrea.prestini@gmail.com>
2023-12-13 01:37:00 +0100

    mod: Pollini REACT
    App.jsx change for todo list

--- Pollini/react-web/react-todo-list-yt/src/App.jsx
+++ Pollini/react-web/react-todo-list-yt/src/App.jsx
@@ -1,19 +1,46 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleNewTodoChange = (event) => {
+    setNewTodo(event.target.value);
+    console.log(newTodo);
+  };
+
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    if (newTodo === "") return;
+    setTodos([...todos, newTodo]);
+    setNewTodo("");
+    console.log(todos);
+  };
+
+  const hadleTodoDelete = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <>
       <h1>Todo List</h1>
       <div className="card">
         <form>
-          <input type="text" />
-          <button type="submit">Add Todo</button>
+          <input type="text" value={newTodo} onChange={handleNewTodoChange} />
+          <button type="submit" onClick={handleAddTodo}>
+            Add Todo
+          </button>
         </form>
         <ul className="todos">
-          <li className="todo">
-            <span>Todo Name</span>
-            <button>Delete</button>
-          </li>
+          {todos.map((todo, index) => (
+            <li className="todo" key={index}>
+              <span>{todo}</span>
+              <button onClick={() => hadleTodoDelete(index)}>Delete</button>
+            </li>
+          ))}
         </ul>
       </div>
     </>
