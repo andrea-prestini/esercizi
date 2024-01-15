5d340c013480aba47b12f04ffa135f8a4255965e
Andrea Prestini <andrea.prestini@gmail.com>
2024-01-15 11:20:16 +0100

    ref code

--- generale/generatorVSlist.py
+++ generale/generatorVSlist.py
@@ -4,16 +4,16 @@ import time
 numero = 1000000
 numbers = [x for x in range(numero)]
 
-trasformato1 = [x ** 2 +1 for x in numbers if (x ** 2 +1) % 2 == 0]
-trasformato2 = (x ** 2 +1 for x in numbers if (x ** 2 +1) % 2 == 0)
+trasformato1 = [x**2 + 1 for x in numbers if (x**2 + 1) % 2 == 0]
+trasformato2 = (x**2 + 1 for x in numbers if (x**2 + 1) % 2 == 0)
 
 t1 = time.process_time()
 print(sum(trasformato2))
-print(time.process_time()-t1)
+print(time.process_time() - t1)
 print()
 t2 = time.process_time()
 print(sum(trasformato1))
-print(time.process_time()-t2)
+print(time.process_time() - t2)
 print("memory used:")
 print(sys.getsizeof(trasformato1), "lista")
 print(sys.getsizeof(trasformato2), "generatore")
